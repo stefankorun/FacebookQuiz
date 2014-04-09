6aa7894f0e39ef1e3a1ed48b42f5b020d3d74045
Korun <korun_stefan@hotmail.com>
2014-04-09 21:01:27 +0200

    Izmeni vo question.php

--- classes/Question.php
+++ classes/Question.php
@@ -3,18 +3,25 @@
 require_once("Answer.php");
 
 class Question {
-    private $questionID;
+    private $id;
+    public $text;
     public $answers;
 
-    public function __construct() {
-        $this->questionID = '-1';
-        $this->answers = array();
-
-        array_push($this->answers, new Answer());
-        array_push($this->answers, new Answer());
-        array_push($this->answers, new Answer());
-        array_push($this->answers, new Answer());
-
+    public function __construct($data) {
+        $this->id = $data["id"];
+        $this->text = $data["text"];
+        $this->answers = $this->parseAnswers($data["answers"]);
     }
 
-} 
+    private function parseAnswers($data) {
+        $answers = array();
+        $dec_data = json_decode($data);
+
+        $dec_data = $dec_data->answers;
+        foreach($dec_data as $a) {
+            $answers[] = new Answer($a);
+        }
+        return $answers;
+    }
+
+}
